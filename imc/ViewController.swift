bcffbcd70d120135764ea7b08fe78f34fe7dfb87
ios8043 <ios8043@mac-mini-de-caelum-4.local>
2019-02-09 15:44:54 -0200

    Initial Commit

---
+++ imc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imc
+//
+//  Created by ios8043 on 09/02/19.
+//  Copyright © 2019 Elton Buscariol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
